a3e03ee9efbffd3ba04474ca1c0fbad040d0e5ce
jinho6225 <jinho6225@hotmail.com>
2020-08-20 16:24:24 -0700

    taskbox UI

---
+++ storybook-static/precache-manifest.98bcc6559f765deafb4f8ef567ecd0ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0f25ee8efc5fada71744c66c46d7fa",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d6a5a015de0075f6a63d.bundle.js"
+  },
+  {
+    "url": "runtime~main.d6a5a015de0075f6a63d.bundle.js"
+  },
+  {
+    "revision": "4e3d0b74a603e5ab18a8",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.d6a5a015de0075f6a63d.bundle.js"
+  },
+  {
+    "url": "vendors~main.d6a5a015de0075f6a63d.bundle.js.LICENSE.txt"
+  }
+]);
